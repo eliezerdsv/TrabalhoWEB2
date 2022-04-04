fa0975adb64677745b6dc51376830282c85c1331
eliezerdsv <eliezerdsv@gmail.com>
2022-04-04 20:39:44 -0300

    outros metodos

--- secauth/src/main/java/dw/secauth/controller/ArtigoControle.java
+++ secauth/src/main/java/dw/secauth/controller/ArtigoControle.java
@@ -2,6 +2,9 @@ package dw.secauth.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import dw.secauth.repository.ArtigoRepository;
 
@@ -17,5 +20,23 @@ public class ArtigoControle {
     public String artigos(Model model){
         model.addAttribute("listaArtigos", rep.findAll());
         return "lista";
+    }
+
+    @GetMapping("/artigos/novo")
+    public String novoArtigo(@ModelAttribute("artigo"), Artigo artigo);
+        return "form";
+    }
+
+    @GetMapping("/artigos/{id}")
+    public String alterarArtigo(@PathVariable("id") long id, Model model){
+        rep.findById(id);
+    }
+        
+    
+
+    @PostMapping("/artigos/salvar")
+    public String salvarArtigo(@ModelAttribute("artigo"), Artigo artigo);
+    rep.save(artigo);    
+    return "redirect:/artigos";
     }
 }
