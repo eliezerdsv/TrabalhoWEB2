3429487552401804d558a1d42aba952c0353f587
eliezerdsv <eliezerdsv@gmail.com>
2022-04-04 19:25:07 -0300

    ListarArtigos

---
+++ secauth/src/main/java/dw/secauth/controller/ArtigoControle.java
@@ -0,0 +1,20 @@
+package dw.secauth.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import dw.secauth.repository.ArtigoRepository;
+
+@Controller
+public class ArtigoControle {
+    private ArtigoRepository rep;
+    public ArtigoControle(ArtigoRepository repository){
+        this.rep = repository;
+    }
+
+    @GetMapping("/artigos")
+    public String artigos(Model model){
+        model.addAttribute("listaArtigos",rep.findAll());
+        return "/artigos/lista";
+    }
+}
