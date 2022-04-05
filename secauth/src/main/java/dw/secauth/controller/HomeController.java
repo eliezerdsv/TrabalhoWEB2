de908c24b650c2e9462f272ac5a7551a6b57a560
eliezerdsv <eliezerdsv@gmail.com>
2022-04-05 15:09:09 -0300

    ultima atualizacao

--- secauth/src/main/java/dw/secauth/controller/HomeController.java
+++ secauth/src/main/java/dw/secauth/controller/HomeController.java
@@ -1,5 +1,6 @@
 package dw.secauth.controller;
 
+import org.springframework.security.core.Authentication;
 //import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,9 @@ class HomeController {
      *      você será redirecionado para a tela de login novamente.
      */
     @GetMapping("/")
-    String index(/*Authentication a*/) {
-      //  if (a != null)
+    String index(Authentication a) {
+        if (a != null)
             return "index.html";
-        //return "redirect:oauth2/authorization/cognito";
+        return "redirect:oauth2/authorization/cognito";
     }    
 }
